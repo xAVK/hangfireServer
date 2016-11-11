2d84b1ee107da79c866305a56bbd470f058ed71a
aleksey.kamenev <aleksey.kamenev@ms2w.com>
2016-11-11 16:30:57 +0500

    initial project version

---
+++ HangServer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hangfire;
+using Hangfire.SqlServer;
+using HangLibrary;
+
+namespace HangServer
+{
+    /*    
+        К проекту подключена библиотека "HangLibrary". В ней содержатся методы, которые использует клиент. Иначе сервер не может их десериализовать
+         */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Press \r\n1 for \"critical\" or\r\n2 for \"default\"");
+            char key = Console.ReadKey().KeyChar;
+            string table = "";
+            switch (key)
+            {
+                case '1': table = "critical"; break;
+                case '2': table = "default"; break;
+                default: table = "default"; break;
+            }
+            GlobalConfiguration.Configuration.UseSqlServerStorage("Data Source=.\\SQLEXPRESS;Initial Catalog=HangFireTest;Integrated Security=True;MultipleActiveResultSets=True");
+            var options = new BackgroundJobServerOptions { Queues = new[] { table } };
+            using (var server = new BackgroundJobServer(options))
+            {
+                Console.WriteLine("\r\nHangfire Server started. Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ HangServer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HangServer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("HangServer")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7db4a135-8730-4761-b385-a1b3e1c59f7a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
