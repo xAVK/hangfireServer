251678eb332d212c1f93f8fd8aa3feb082326631
aleksey.kamenev <aleksey.kamenev@ms2w.com>
2017-01-30 17:15:10 +0500

    head

--- HangServer/Program.cs
+++ HangServer/Program.cs
@@ -16,18 +16,19 @@ namespace HangServer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Press \r\n1 for \"critical\" or\r\n2 for \"default\"");
-            char key = Console.ReadKey().KeyChar;
-            string table = "";
-            switch (key)
-            {
-                case '1': table = "critical"; break;
-                case '2': table = "default"; break;
-                default: table = "default"; break;
-            }
+            //Console.WriteLine($"Press \r\n1 for \"critical\" or\r\n2 for \"default\"");
+            //char key = Console.ReadKey().KeyChar;
+            //string table = "";
+            //switch (key)
+            //{
+            //    case '1': table = "critical"; break;
+            //    case '2': table = "default"; break;
+            //    default: table = "default"; break;
+            //}
+
             GlobalConfiguration.Configuration.UseSqlServerStorage("Data Source=.\\SQLEXPRESS;Initial Catalog=HangFireTest;Integrated Security=True;MultipleActiveResultSets=True");
-            var options = new BackgroundJobServerOptions { Queues = new[] { table } };
-            using (var server = new BackgroundJobServer(options))
+            //var options = new BackgroundJobServerOptions { Queues = new[] { table } };
+            using (var server = new BackgroundJobServer(/*options*/))
             {
                 Console.WriteLine("\r\nHangfire Server started. Press any key to exit...");
                 Console.ReadKey();
